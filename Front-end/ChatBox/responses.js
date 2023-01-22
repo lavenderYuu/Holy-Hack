9bc6d6b5ab6817c051370e1355a5ba745f2187ad
lavenderYuu <lavenderyu0113@gmail.com>
2023-01-22 00:29:15 -0800

    modified:   Front-end/ChatBox/chatTest.css 	modified:   Front-end/ChatBox/responses.js

--- Front-end/ChatBox/responses.js
+++ Front-end/ChatBox/responses.js
@@ -1,12 +1,4 @@
 function getBotResponse(input) {
-    //rock paper scissors
-    if (input == "rock") {
-        return "paper";
-    } else if (input == "paper") {
-        return "scissors";
-    } else if (input == "scissors") {
-        return "rock";
-    }
 
     // Simple responses
     if (input == "hello") {
