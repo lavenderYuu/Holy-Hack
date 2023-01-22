89d5a878c55f631121f996dc5476d3eea804f27b
wla194 <wla194@sfu.ca>
2023-01-22 07:48:13 -0800

    Connect templates to backend

---
+++ static/makePartner.js
@@ -0,0 +1,14 @@
+
+function makePtner(){
+    console.log(document.cookie)
+    prompts = document.getElementById("Prompts").value
+    gender = document.getElementById("genderTab").innerText
+    fetch('http://127.0.0.1:5000/image', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"prompts": gender + " person with " + prompts})
+    }).then(response => window.location.assign("http://127.0.0.1:5000/chatbox"))
+}
